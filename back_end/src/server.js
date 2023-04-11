f8f6ea9a3e85d37e349d326b6902cec8865ec6e6
abir2301 <abirchalghamie@gmail.com>
2023-04-11 01:25:18 +0100

    login issues

--- back_end/src/server.js
+++ back_end/src/server.js
@@ -4,13 +4,26 @@ require("dotenv").config();
 const users = require("./routes/user");
 const profiles = require("./routes/profile.route");
 const healthProblem = require("./routes/hp.route");
-
+const os = require("os");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 dbConnection.connect();
 
 app.get("/", (req, res) => {
+  const networkInterfaces = os.networkInterfaces();
+  const interfaces = Object.values(networkInterfaces);
+  let ipAddress = "";
+
+  interfaces.forEach((interfaceArray) => {
+    interfaceArray.forEach((networkInterface) => {
+      if (networkInterface.family === "IPv4" && !networkInterface.internal) {
+        ipAddress = networkInterface.address;
+        console.log(ipAddress);
+      }
+    });
+  });
+
   res.send("heyy appiii");
 });
 app.use("/api/users", users);
@@ -18,7 +31,9 @@ app.use("/api/profiles", profiles);
 app.use("/api/hp", healthProblem);
 
 const port = process.env.PORT ? process.env.PORT : 3000;
-const ipAddress = "192.168.1.43";
+
+const ipAddress = process.env.IP_ADDRESS ? process.env.IP_ADDRESS : "0.0.0.0";
+
 app.listen(port, ipAddress, () => {
   console.log("app run on port ", port, "address ", ipAddress);
 });
